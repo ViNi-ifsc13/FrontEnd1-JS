1b4c651cfe5c4243e6822ee37b8b9661b78af29a
ViNi-ifsc13 <vinicius.bs11@aluno.ifsc.edu.br>
2026-04-14 01:07:46 +0000

    Aula 1 js

---
+++ lista-01.js
@@ -0,0 +1,39 @@
+// Questões
+
+// 1
+
+function soma(){
+    let a = new Array();
+
+    for (let i = 1; i < a.length; i++) {
+        const soma = a[i - 1] + a[i + 1];        
+    }
+    
+
+
+
+}
+
+// 2
+
+// 3
+
+// 4
+
+// 5
+
+// 6
+
+// 7
+
+// 8
+
+// 9
+
+// 10
+
+// 11
+
+// 12
+
+// 13
